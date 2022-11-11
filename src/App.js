2888d29b88018ec1ccf8b52f1786e5d1eafa928d
YUJOO-LEE <lllllllllee@gmail.com>
2022-11-11 11:21:13 +0900

    광원 설정

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './scss/style.scss';
 //import Orbit from './components/Orbit';
 import { Canvas, extend, useFrame, useThree } from 'react-three-fiber';
 import { useRef } from 'react';
+import { meshPhongMaterial } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 extend({ OrbitControls});
 
@@ -11,6 +12,20 @@ const Orbit = ()=>{
 	return <orbitControls args={[camera, gl.domElement]} />
 }
 
+const Bulb = (props)=>{
+	return(
+		<mesh {...props}>
+			<pointLight 
+				castShadow
+				intensity={1}
+			/>
+			{/* 반지금, 가로면분할, 세로면분할 */}
+			<sphereBufferGeometry args={[0.5, 20, 20]} />
+			<meshPhongMaterial emissive='yellow'/>
+		</mesh>
+	);
+}
+
 const Box = (props)=>{
 	const ref = useRef(null);
 	useFrame(()=>{
@@ -18,6 +33,8 @@ const Box = (props)=>{
 	})
 	return (
 		<mesh ref={ref} {...props}
+			receiveShadow
+			castShadow
 		>
 			<boxGeometry />
 			<meshPhysicalMaterial color='blue' />
@@ -34,17 +51,19 @@ const Floor = (props)=>{
 	);
 }
 
+
 function App() {
 	return (
 		<figure>
 			<Canvas // shadowMap 
 				style={{background: '#111'}}
-				camera={{position:[3,3,3]}}	// x, y, z
+				camera={{position:[3, 3, 3]}}	// x, y, z
 			>
 				<axesHelper args={[6]} />
 				<Orbit />
 
 				<ambientLight intensity={0.2} />
+				<Bulb position={[0, 3, 0]} />
 				<Box position={[-1, 1, 2]} />
 				<Floor position={[0, -0.5, 0]} />
 			</Canvas>
