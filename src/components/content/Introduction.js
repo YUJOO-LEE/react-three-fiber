d979f25007ca2429c90df8c9273ec40dda2ca62b
YUJOO-LEE <lllllllllee@gmail.com>
2022-11-29 18:31:05 +0900

    introduction 페이지 내용 추가

--- src/components/content/Introduction.js
+++ src/components/content/Introduction.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
 function Introduction() {
+  const baseUrl = process.env.PUBLIC_URL;
+
   return (
     <div className='inner introduction'>
-      <h2>Consect</h2>
+      <h2>Introduction</h2>
       <div>
-        <h3></h3>
-        <p></p>
+        <div className='profile'>
+          <ul>
+            <li>Director</li>
+            <li>Yujoo Lee</li>
+            <li>lllllllllee@gmail.com</li>
+          </ul>
+          <div className='imgWrap'>
+            <img src={`${baseUrl}/img/director.png`} alt='director' />
+          </div>
+        </div>
+        <p>Generally, the sole superiors of the director are the producers and the studio that is financing the film, although sometimes the director can also be a producer of the same film. The role of a director differs from producers in that producers typically manage the logistics and business operations of the production, whereas the director is tasked with making creative decisions. The director must work within the restrictions of the film's budget[14] and the demands of the producer and studio (such as the need to get a particular age rating)</p>
       </div>
     </div>
   )
