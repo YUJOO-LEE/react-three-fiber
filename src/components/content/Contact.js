0d77cf5db338c30c3e9e68cb84c662413050b90c
YUJOO-LEE <lllllllllee@gmail.com>
2022-11-29 19:51:43 +0900

    reference, contact 내용 추가, 세로 스크롤 지정

--- src/components/content/Contact.js
+++ src/components/content/Contact.js
@@ -4,6 +4,26 @@ function Contact() {
   return (
     <div className='inner contact'>
       <h2>Contact</h2>
+      <form action="#">
+        <ul>
+          <li>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" required />
+          </li>
+          <li>
+            <label htmlFor="title">Title</label>
+            <input type="text" name="title" id="title" required/>
+          </li>
+          <li>
+            <label htmlFor="content">Content</label>
+            <textarea name="content" id="content" cols="30" rows="5" required></textarea>
+          </li>
+          <li className='btns'>
+            <input type="reset" value="RESET" />
+            <input type="submit" value="SEND" />
+          </li>
+        </ul>
+      </form>
     </div>
   )
 }
--- src/components/content/Reference.js
+++ src/components/content/Reference.js
@@ -4,6 +4,28 @@ function Reference() {
   return (
     <div className='inner reference'>
       <h2>Reference</h2>
+      <ul>
+        <li>
+          <p>Pickard’s manual of operative dentistry. 9th edn. Oxford: Oxford University Press.</p>
+          <p>Banerjee, A. and Watson, T.F. (2011)</p>
+        </li>
+        <li>
+          <p>The view across the river: Harriette Colenso and the Zulu struggle against imperialism. Charlottesville, Virginia: University Press of Virginia.</p>
+          <p>Guy, J. (2001)</p>
+        </li>
+        <li>
+          <p>The Iliad. Translated by J. Davies. Introduction and notes by D. Wright. London: Dover Publications.</p>
+          <p>Homer (1997)</p>
+        </li>
+        <li>
+          <p>The wonders of the Universe. 2nd edn. Edited by Frederick Jones, James Smith and Tony Bradley. London: Smiths.</p>
+          <p>Lucas, G. (2004)</p>
+        </li>
+        <li>
+          <p>2nd edn. London: British Medical Association.</p>
+          <p>Medicine in old age (1985)</p>
+        </li>
+      </ul>
     </div>
   )
 }
